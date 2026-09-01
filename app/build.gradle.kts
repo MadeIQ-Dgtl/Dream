0954edd664d2e6b3dc497fee8f405dd9008eee6c
MadeIQ-Dgtl <lubos.csordas@yahoo.com>
2026-09-01 20:41:15 +0200

    Prepare DREAM 1.0.1 build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "digital.madeiq.dream"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0-test1"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
